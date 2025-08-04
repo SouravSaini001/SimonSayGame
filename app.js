d7e3049d1213dfc076c7f8c2afb73265366907fa
Sourav Saini <compit458@gmail.com>
2025-08-04 14:29:31 +0530

    update js

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ let started = false;
 let level = 0;
 let score = 0;
 let h2 = document.querySelector("h2");
-let btns = ["red","green","yellow","blue"];
+let btns = ["red", "green", "yellow", "blue"];
 
-document.addEventListener("keypress",function() {
-    if(started == false) {
+document.addEventListener("keypress", function () {
+    if (started == false) {
         console.log("Game Started.");
         started = true;
         levelUp();
@@ -16,11 +16,22 @@ document.addEventListener("keypress",function() {
 });
 
 function btnFlast(btn) {
+    // Get color name from button id
+    let color = btn.id;
+
+    // Create and play the .wav audio
+    let audio = new Audio(`sounds/${color}.wav`);
+    audio.play();
+
+    // Flash effect
     btn.classList.add("flash");
-    setTimeout(function() {
+    setTimeout(function () {
         btn.classList.remove("flash");
-    },250);
+
+
+    }, 250);
 }
+
 
 function levelUp() {
     userSeq = [];
@@ -28,7 +39,7 @@ function levelUp() {
     score++;
     h2.innerText = `Level ${level}`;
     // Selecting Random Button to flash.. 
-    let randIdx = Math.floor(Math.random()*4);
+    let randIdx = Math.floor(Math.random() * 4);
     let randColor = btns[randIdx];
     gameSeq.push(randColor);
     console.log(gameSeq);
@@ -37,25 +48,28 @@ function levelUp() {
 }
 
 function gameOver() {
-    setTimeout(function() {
+    setTimeout(function () {
         let body = document.querySelector("body");
         body.style.backgroundColor = "aqua";
-    },250);
+    }, 250);
     userSeq = [];
     gameSeq = [];
     started = false;
     level = 0;
     score = 0;
-    
+
 }
 
 function checkResult(idx) {
-    if(userSeq[idx] == gameSeq[idx]) {
-        if(userSeq.length == gameSeq.length) {
-            setTimeout(levelUp,1000);
-        }  
-    }else {
+    if (userSeq[idx] == gameSeq[idx]) {
+        if (userSeq.length == gameSeq.length) {
+            setTimeout(levelUp, 1000);
+        }
+    } else {
         h2.innerHTML = `<h2>Game Over! Your score was ${score} <br>  Press any key to start.<h2>`;
+        // Create and play the .wav audio
+        let audio = new Audio(`sounds/gameover.mp3`);
+        audio.play();
         let body = document.querySelector("body");
         body.style.backgroundColor = "red";
         gameOver();
@@ -65,36 +79,36 @@ function checkResult(idx) {
 
 
 function btnPress() {
-    
+
     btnFlast(this);
 
     let userColor = this.id;
     userSeq.push(userColor);
-    
+
 
     checkResult(userSeq.length - 1);
 }
 
 let allBtns = document.querySelectorAll(".btn");
-for(btn of allBtns) {
-    btn.addEventListener("click",btnPress);
+for (btn of allBtns) {
+    btn.addEventListener("click", btnPress);
 }
 
-setTimeout(function() {
+setTimeout(function () {
     let hint = document.querySelector(".hintBtn");
-    hint.addEventListener("click",function() {
+    hint.addEventListener("click", function () {
         this.style.opacity = "0";
         let h3 = document.createElement("h3");
         let container = document.querySelector(".hint");
-        let hintData  = gameSeq.toString();
+        let hintData = gameSeq.toString();
         h3.innerText = hintData;
-        container.appendChild(h3); 
-        setTimeout(function() {
+        container.appendChild(h3);
+        setTimeout(function () {
             h3.remove();
             let hint = document.querySelector(".hintBtn");
             hint.style.opacity = '1';
-        },1500);
+        }, 1500);
     });
-},1000);
+}, 1000);
 
 
